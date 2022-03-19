5c963fa4a3c04a3f5c6f7de6d9dbdfe7dda570d7
jet <646032852@qq.com>
2022-03-19 09:16:47 +0800

    并发编程第10章 避免活跃性危险

--- src/main/java/com/jetluo/jcip/chapter10/LeftRightDeadlock.java
+++ src/main/java/com/jetluo/jcip/chapter10/LeftRightDeadlock.java
@@ -2,7 +2,9 @@ package com.jetluo.jcip.chapter10;
 
 /**
  * @ClassName LeftRightDeadlock
- * @Description TODO
+ *  程序清单 10-1
+ *  简单的锁顺序死锁（不要这样做）
+ * @Description
  * @Author jet
  * @Date 2022/3/19 09:15
  * @Version 1.0
